00eb7738d71b72fa962f02abae46b1107ebffa21
PoonamGhodake05 <poonam@intellectlogic.in>
2018-12-05 12:52:26 +0530

    Test

--- app/src/main/java/com/example/ilspl/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ilspl/myapplication/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.ilspl.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
+//https://github.com/PoonamGhodake05/MyApplication/settings
 public class MainActivity extends AppCompatActivity {
 TextView tv_click;
     @Override
@@ -13,13 +14,16 @@ TextView tv_click;
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv_click= (TextView)findViewById(R.id.tv_click);
+        tv_click= findViewById(R.id.tv_click);
         tv_click.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
                 Toast.makeText(getApplicationContext(),"Action clicked",Toast.LENGTH_LONG).show();
+            //goto second activity from main activity using intent
+                Intent it=new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(it);
             }
         });
 
