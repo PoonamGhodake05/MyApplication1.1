d7ec6ea8d95105659bc4ce3cbd508e621731988c
PoonamGhodake05 <poonam@intellectlogic.in>
2018-12-05 12:12:58 +0530

    Test

--- app/src/main/java/com/example/ilspl/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ilspl/myapplication/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.ilspl.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView tv_click;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_click= findViewById(R.id.tv_click);
+        tv_click.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(getApplicationContext(),"Action clicked",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
