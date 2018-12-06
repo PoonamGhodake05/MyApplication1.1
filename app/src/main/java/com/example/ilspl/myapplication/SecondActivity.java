165aa6aeb767a2ed1d08add3ae5a4af8d2bbecca
PoonamGhodake05 <poonam@intellectlogic.in>
2018-12-06 13:50:04 +0530

    Test

--- app/src/main/java/com/example/ilspl/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/ilspl/myapplication/SecondActivity.java
@@ -2,15 +2,18 @@ package com.example.ilspl.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity
 {
-
+TextView tv_display;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        tv_display=findViewById(R.id.tv_display);
+        tv_display.setText("Second Page");
 
     }
 }
